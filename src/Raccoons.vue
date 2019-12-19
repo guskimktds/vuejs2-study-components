7918000306cf2ddcbd3b2d515c8a14ed94ed6afa
Yongeon Kim <gusraccoon@gmail.com>
2019-12-19 17:15:53 +0900

    first vue components commit

---
+++ src/Raccoons.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>List of Raccoons</h1>
+    <ul>
+      <li v-for="raccoon in raccoons">{{ raccoon }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      raccoons: ['Kim','Oho','Lee']
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: green;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+// import Raccoons from './Raccoons.vue'
+//
+// Vue.component('raccoons', Raccoons);
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
